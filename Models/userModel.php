153e9a32a5d66a71fc907b0bc6a9b12444e314ba
Matthis <matthisgastesoyer@gmail.com>
2024-11-18 20:26:36 +0100

    Updated userModel for login functions, updated functions & vars types

--- Models/userModel.php
+++ Models/userModel.php
@@ -1,7 +1,7 @@
 <?php
 
 class UserModel {
-    private $bdd;
+    private PDO $bdd;
 
     public function __construct($host, $dbname, $user, $password) {
         try {
@@ -24,15 +24,24 @@ class UserModel {
         return $req->fetch();
     }
 
-    public function addUser($email, $motdepasse, $grp_tp_user) {
+    public function addUser($email, $motdepasse, $grp_tp_user): void
+    {
         $req = $this->bdd->prepare('INSERT INTO utilisateur (Mail_User, Mdp_User, Grp_TP_User) VALUES (:email, :motdepasse, :grp_tp_user)');
         $req->execute(array('email' => $email, 'motdepasse' => password_hash($motdepasse, PASSWORD_DEFAULT), 'grp_tp_user' => $grp_tp_user));
     }
 
-    public function emailExists($email) {
+    public function emailExists($email): bool
+    {
         $req = $this->bdd->prepare('SELECT Mail_User FROM utilisateur WHERE Mail_User = :email');
         $req->execute(array('email' => $email));
         return $req->rowCount() > 0;
+    }
+
+    public function passwordIsValid($password): bool
+    {
+        $req = $this->bdd->prepare('SELECT Mdp_User FROM utilisateur WHERE Mdp_User = :password');
+        $req->execute(array('password' => $password));
+        return password_verify($password, $req->fetch()['Mdp_User']);
     }
 }
 ?>
