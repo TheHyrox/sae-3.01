0f3ccfa77be7f0fb654e5d2ff7675774a43199ed
Matthis <matthisgastesoyer@gmail.com>
2024-11-19 13:22:32 +0100

    Create productModel & change userModel

--- Models/userModel.php
+++ Models/userModel.php
@@ -1,38 +1,38 @@
 <?php
 
 class UserModel {
-    private PDO $bdd;
+    private PDO $db;
 
     public function __construct($host, $dbname, $user, $password) {
         try {
-            $this->bdd = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
-            $this->bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->db = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+            $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             die("Erreur de connexion : " . $e->getMessage());
         }
     }
 
     public function getUser($id) {
-        $req = $this->bdd->prepare('SELECT * FROM utilisateur WHERE Id_User = :id');
+        $req = $this->db->prepare('SELECT * FROM utilisateur WHERE Id_User = :id');
         $req->execute(array('id' => $id));
         return $req->fetch();
     }
 
     public function getUserByEmail($email) {
-        $req = $this->bdd->prepare('SELECT * FROM utilisateur WHERE Email = :email');
+        $req = $this->db->prepare('SELECT * FROM utilisateur WHERE Email = :email');
         $req->execute(array('email' => $email));
         return $req->fetch();
     }
 
     public function addUser($email, $motdepasse, $grp_tp_user): void
     {
-        $req = $this->bdd->prepare('INSERT INTO utilisateur (Mail_User, Mdp_User, Grp_TP_User) VALUES (:email, :motdepasse, :grp_tp_user)');
+        $req = $this->db->prepare('INSERT INTO utilisateur (Mail_User, Mdp_User, Grp_TP_User) VALUES (:email, :motdepasse, :grp_tp_user)');
         $req->execute(array('email' => $email, 'motdepasse' => password_hash($motdepasse, PASSWORD_DEFAULT), 'grp_tp_user' => $grp_tp_user));
     }
 
     public function emailExists($email): bool
     {
-        $stmt = $this->bdd->prepare("SELECT COUNT(*) FROM utilisateur WHERE Mail_User = :email");
+        $stmt = $this->db->prepare("SELECT COUNT(*) FROM utilisateur WHERE Mail_User = :email");
         $stmt->bindParam(':email', $email);
         $stmt->execute();
         return $stmt->fetchColumn() > 0;
@@ -40,7 +40,7 @@ class UserModel {
 
     public function passwordIsValid($email, $password): bool
     {
-        $stmt = $this->bdd->prepare("SELECT Mdp_User FROM utilisateur WHERE Mail_User = :email");
+        $stmt = $this->db->prepare("SELECT Mdp_User FROM utilisateur WHERE Mail_User = :email");
         $stmt->bindParam(':email', $email);
         $stmt->execute();
         $hashedPassword = $stmt->fetchColumn();
@@ -49,7 +49,7 @@ class UserModel {
 
     public function isAdmin($email): bool
     {
-        $stmt = $this->bdd->prepare("SELECT Niv_Acces_User FROM utilisateur WHERE Mail_User = :email");
+        $stmt = $this->db->prepare("SELECT Niv_Acces_User FROM utilisateur WHERE Mail_User = :email");
         $stmt->bindParam(':email', $email);
         $stmt->execute();
         return $stmt->fetchColumn() < 4;
