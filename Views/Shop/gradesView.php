9326531f885fbaa2954bbe54e58a0d2c07b36eb7
Matthis <matthisgastesoyer@gmail.com>
2024-11-21 15:43:08 +0100

    Created views for grades & products

---
+++ Views/Shop/gradesView.php
@@ -0,0 +1,19 @@
+<?php
+foreach ($grades as $row) {
+    if (isset($row['Id_Grade'])) {
+        echo "<article>
+                    <h2>" . htmlspecialchars($row['Nom_Grade']) . "</h2>
+                    <div class='imgBackground'><img src='../Pictures/" . htmlspecialchars($row['URL_Img_Grade']) . "' alt=''></div>
+                    <p>" . htmlspecialchars($row['Desc_Grade']) . "</p>
+                    <div class='row'>
+                        <form action='checkout.php' method='post'>
+                            <input type='hidden' name='name' value='" . htmlspecialchars($row['Nom_Grade']) . "'>
+                            <input type='hidden' name='price' value='" . htmlspecialchars($row['Prix_Grade']) . "'>
+                            <input type='hidden' name='quantity' value='1'>
+                            <input type='submit' name='add_to_cart' value='Acheter'>
+                        </form>
+                        <h3>" . htmlspecialchars($row['Prix_Grade']) . "€</h3>
+                    </div>
+                  </article>";
+    }
+}
---
+++ Views/Shop/productsView.php
@@ -0,0 +1,15 @@
+<?php
+echo "<article>
+                    <h2>" . htmlspecialchars($row['Nom_Produit']) . "</h2>
+                    <div class='imgBackground'><img src='../Pictures/" . htmlspecialchars($row['URL_Img_Produit']) . "' alt=''></div>
+                    <p>" . htmlspecialchars($row['Desc_Produit']) . "</p>
+                    <div class='row'>
+                        <form action='checkout.php' method='post'>
+                            <input type='hidden' name='name' value='" . htmlspecialchars($row['Nom_Produit']) . "'>
+                            <input type='hidden' name='price' value='" . htmlspecialchars($row['Prix_Produit']) . "'>
+                            <input type='hidden' name='quantity' value='1'>
+                            <input type='submit' name='add_to_cart' value='Acheter'>
+                        </form>
+                        <h3>" . htmlspecialchars($row['Prix_Produit']) . "€</h3>
+                    </div>
+                  </article>";
