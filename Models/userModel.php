60a10555995e71b171a1da2deddc1ff0653cfb38
Matthis <matthisgastesoyer@gmail.com>
2024-11-22 12:32:21 +0100

    Update userModel for new db

--- Models/userModel.php
+++ Models/userModel.php
@@ -10,31 +10,31 @@ class UserModel {
     }
 
     public function getUser($id) {
-        $req = $this->db->prepare('SELECT * FROM utilisateur WHERE Id_User = :id');
+        $req = $this->db->prepare('SELECT * FROM UTILISATEUR WHERE Id_User = :id');
         $req->execute(array('id' => $id));
         return $req->fetch();
     }
 
     public function getUserByEmail($email) {
-        $req = $this->db->prepare('SELECT * FROM utilisateur WHERE Email = :email');
+        $req = $this->db->prepare('SELECT * FROM UTILISATEUR WHERE Email = :email');
         $req->execute(array('email' => $email));
         return $req->fetch();
     }
 
     public function addUser($email, $motdepasse, $grp_tp_user): void {
-        $req = $this->db->prepare('INSERT INTO utilisateur (Mail_User, Mdp_User, Grp_TP_User) VALUES (:email, :motdepasse, :grp_tp_user)');
+        $req = $this->db->prepare('INSERT INTO UTILISATEUR (Mail_User, Mdp_User, Grp_TP_User) VALUES (:email, :motdepasse, :grp_tp_user)');
         $req->execute(array('email' => $email, 'motdepasse' => password_hash($motdepasse, PASSWORD_DEFAULT), 'grp_tp_user' => $grp_tp_user));
     }
 
     public function emailExists($email): bool {
-        $stmt = $this->db->prepare("SELECT COUNT(*) FROM utilisateur WHERE Mail_User = :email");
+        $stmt = $this->db->prepare("SELECT COUNT(*) FROM UTILISATEUR WHERE Mail_User = :email");
         $stmt->bindParam(':email', $email);
         $stmt->execute();
         return $stmt->fetchColumn() > 0;
     }
 
     public function passwordIsValid($email, $password): bool {
-        $stmt = $this->db->prepare("SELECT Mdp_User FROM utilisateur WHERE Mail_User = :email");
+        $stmt = $this->db->prepare("SELECT Mdp_User FROM UTILISATEUR WHERE Mail_User = :email");
         $stmt->bindParam(':email', $email);
         $stmt->execute();
         $hashedPassword = $stmt->fetchColumn();
@@ -42,14 +42,14 @@ class UserModel {
     }
 
     public function isAdmin($email): bool {
-        $stmt = $this->db->prepare("SELECT Niv_Acces_User FROM utilisateur WHERE Mail_User = :email");
+        $stmt = $this->db->prepare("SELECT Niv_Acces_User FROM UTILISATEUR WHERE Mail_User = :email");
         $stmt->bindParam(':email', $email);
         $stmt->execute();
         return $stmt->fetchColumn() < 4;
     }
 
     public function getGrpTPUser($email): ?string {
-        $stmt = $this->db->prepare("SELECT Grp_TP_User FROM utilisateur WHERE Mail_User = :email");
+        $stmt = $this->db->prepare("SELECT Grp_TP_User FROM UTILISATEUR WHERE Mail_User = :email");
         $stmt->bindParam(':email', $email);
         $stmt->execute();
         return $stmt->fetchColumn();
