668ef63b9361fe76f369582be96b7866a9ab634d
Brokuto <louann.gransart13@gmail.com>
2024-11-22 10:53:24 +0100

    fichier fini

--- HTML/file.php
+++ HTML/file.php
@@ -15,26 +15,26 @@
         <div class="folderfond">
             <div class="folder">
             <a href="" class="image-folder">
-                <img src="../Icons/folder.png" alt="Folder">
+                <img src="../Icons/folder.svg" alt="Folder">
             </a>
                 <span>Dossier 1</span>
             </div>
             <!-- Duplicate the "folder" div for additional folders -->
             <div class="folder">
             <a href="" class="image-folder">
-                <img src="../Icons/folder.png" alt="Folder">
+                <img src="../Icons/folder.svg" alt="Folder">
             </a>
                 <span>Dossier 2</span>
             </div>
             <div class="folder">
             <a href="" class="image-folder">
-                <img src="../Icons/folder.png" alt="Folder">
+                <img src="../Icons/folder.svg" alt="Folder">
             </a>
                 <span>Dossier 3</span>
             </div>
             <div class="file">
             <a href="" class="image-file">
-                <img src="../Icons/file.png" alt="file">
+                <img src="../Icons/file.svg" alt="file">
             </a>
                 <span>Fichier 1</span>
             </div>
