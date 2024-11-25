d27c3daeb0b1e63f5c5572d3e9b2a68ac349f22a
Matthis <matthisgastesoyer@gmail.com>
2024-11-25 09:21:39 +0100

    Update products & grades view - check admin view not if the user is admin

--- Views/Shop/gradesView.php
+++ Views/Shop/gradesView.php
@@ -1,19 +1,37 @@
 <?php
+$isAdmin = isset($_SESSION['isAdminView']) && $_SESSION['isAdminView'] === true;
+
 foreach ($grades as $row) {
     if (isset($row['Id_Grade'])) {
         echo "<article>
                     <h2>" . htmlspecialchars($row['Nom_Grade']) . "</h2>
                     <div class='imgBackground'><img src='../../Pictures/Uploads/" . htmlspecialchars($row['URL_Img_Grade']) . "' alt=''></div>
                     <p>" . htmlspecialchars($row['Desc_Grade']) . "</p>
-                    <div class='row'>
-                        <form action='checkout.php' method='post'>
-                            <input type='hidden' name='name' value='" . htmlspecialchars($row['Nom_Grade']) . "'>
-                            <input type='hidden' name='price' value='" . htmlspecialchars($row['Prix_Grade']) . "'>
-                            <input type='hidden' name='quantity' value='1'>
-                            <input type='submit' name='add_to_cart' value='Acheter'>
-                        </form>
-                        <h3>" . htmlspecialchars($row['Prix_Grade']) . "€</h3>
+                    <div class='row'>";
+
+        if ($isAdmin) {
+            echo "<form action='' method='post'>
+                        <input type='hidden' name='grade_id' value='" . htmlspecialchars($row['Id_Grade']) . "'>
+                        <input type='submit' id='editGradeButton' 
+                               data-id='" . htmlspecialchars($row['Id_Grade']) . "'
+                               data-name='" . htmlspecialchars($row['Nom_Grade']) . "'
+                               data-description='" . htmlspecialchars($row['Desc_Grade']) . "'
+                               data-price='" . htmlspecialchars($row['Prix_Grade']) . "'
+                               data-img='" . htmlspecialchars($row['URL_Img_Grade']) . "'
+                               value='Modifier'>
+                    </form>";
+        } else {
+            echo "<form action='checkout.php' method='post'>
+                        <input type='hidden' name='name' value='" . htmlspecialchars($row['Nom_Grade']) . "'>
+                        <input type='hidden' name='price' value='" . htmlspecialchars($row['Prix_Grade']) . "'>
+                        <input type='hidden' name='quantity' value='1'>
+                        <input type='submit' name='add_to_cart' value='Acheter'>
+                    </form>";
+        }
+
+        echo "<h3>" . htmlspecialchars($row['Prix_Grade']) . "€</h3>
                     </div>
                   </article>";
     }
 }
+?>
