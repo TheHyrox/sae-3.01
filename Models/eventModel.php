9fc59b47e540d2da0d6af42e39a9533519a2b0fc
Matthis <matthisgastesoyer@gmail.com>
2024-11-27 09:33:43 +0100

    Added addEvent in eventModel

--- Models/eventModel.php
+++ Models/eventModel.php
@@ -16,4 +16,19 @@ class eventModel
         $req->execute();
         return $req->fetchAll(PDO::FETCH_ASSOC);
     }
+
+    public function addEvent($name, $description, $date, $lieu, $cat, $nb_places, $prix, $img): void
+    {
+        $req = $this->db->prepare('INSERT INTO EVENT (Nom_Event, Desc_Event, Date_Event, Lieu_Event, Cat_Event, Nb_Place_Event, Prix_Event, URL_Img_Event) VALUES (:name, :description, :date, :location, :cat, :nb_places, :prix, :img)');
+        $req->execute([
+            'name' => $name,
+            'description' => $description,
+            'date' => $date,
+            'location' => $lieu,
+            'cat' => $cat,
+            'nb_places' => $nb_places,
+            'prix' => $prix,
+            'img' => $img
+        ]);
+    }
 }
