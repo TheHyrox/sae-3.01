dff94300480416fb51da573f11dd809b96fc78b8
Nedaok <152980200+Nedaok@users.noreply.github.com>
2025-01-07 16:23:10 +0100

    accountController

--- Controllers/accountController.php
+++ Controllers/accountController.php
@@ -1,20 +1,47 @@
 <?php
 
 require '../Models/accountModel.php';
-require_once '../Utils/DBConfig/Database.php';
-require_once '../Utils/DBConfig/Config.php';
+require_once '/home/inf2pj06/public_html/Models/userModel.php';
+require '/home/inf2pj06/public_html/Utils/DBConfig/Database.php';
+require '/home/inf2pj06/public_html/Utils/DBConfig/Config.php';
 
 class accountController
 {
-    private accountModel $model;
+    private accountModel $accountModel;
+    private userModel $userModel;
 
     public function __construct()
     {
-        $this->model = new accountModel();
+        $this->accountModel = new accountModel();
+        $this->userModel = new UserModel();
     }
 
     public function getUserEvents($userId): array
     {
-        return $this->model->getUserEvents($userId);
+        return $this->accountModel->getUserEvents($userId);
+    }
+
+    public function getUser($id){
+        return $this->userModel->getUser($id);
+    }
+
+    public function getUserByEmail($email){
+        return $this->userModel->getUserByEmail($email);
+    }
+
+    public function setNom($nom){
+        return $this->userModel->setNom($nom);
+    }
+    public function setPrenom($prenom){
+        return $this->userModel->setPrenom($prenom);
+    }
+    public function setMail($mail){
+        return $this->userModel->setMail($mail);
+    }
+    public function setTP($tp){
+        return $this->userModel->setTP($tp);
+    }
+    public function setMdp($mdp){
+        return $this->userModel->setMdp($mdp);
     }
 }
